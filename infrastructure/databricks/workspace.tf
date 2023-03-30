697a8ee4be6c53a3315fdf68391a62d02215e190
Matko Soric AT <Matko.Soric@alexanderthamm.com>
2023-03-30 21:04:25 +0200

    workspace back

--- infrastructure/databricks/workspace.tf
+++ infrastructure/databricks/workspace.tf
@@ -1,10 +1,10 @@
-#
-#resource "azurerm_databricks_workspace" "db_workspace" {
-#  name                        = "${var.prefix}-workspace" # -${var.environment}
-#  resource_group_name         = var.resource_group
-#  location                    = var.region
-#  sku                         = "premium"
-#  managed_resource_group_name = "${var.prefix}-workspace-rg"
-#  tags                        = var.tags
-#}
-#
+
+resource "azurerm_databricks_workspace" "db_workspace" {
+  name                        = "${var.prefix}-workspace" # -${var.environment}
+  resource_group_name         = var.resource_group
+  location                    = var.region
+  sku                         = "premium"
+  managed_resource_group_name = "${var.prefix}-workspace-rg"
+  tags                        = var.tags
+}
+
